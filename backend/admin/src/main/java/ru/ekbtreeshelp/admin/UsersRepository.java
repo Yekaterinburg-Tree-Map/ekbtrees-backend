3af779f8ae4f7d7a8a77ca2dc7661f280c61c277
evin76 <gengerw1604@gmail.com>
2021-11-18 15:23:48 +0300

    commit 2

--- backend/admin/src/main/java/ru/ekbtreeshelp/admin/UsersRepository.java
+++ backend/admin/src/main/java/ru/ekbtreeshelp/admin/UsersRepository.java
@@ -2,19 +2,28 @@ package ru.ekbtreeshelp.admin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.ekbtreeshelp.core.entity.User;
 
-import java.util.Date;
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    public UserEntity findUserEntityByID(Long id);
+@Repository
+public interface UsersRepository extends JpaRepository<UserEntity, Long> {
 
     @Query("UPDATE UserEntity set blocked = true WHERE id = :id")
-    void blocked();
+    void blockUser();
 
     @Query(nativeQuery = true, value="UPDATE users set is_blocked = true WHERE id = :id")
     void blockUserNative(Long id);
 
     @Query(value = "SELECT * FROM users OFFSET (:pageNumber * :pageSize) LIMIT * :pageSize", nativeQuery = true)
     List<UserData> findUserEntitiesBySomeValueWithPaging(String value, int pageNumber, int pageSize);
+
+    @Query(value = "SELECT * FROM users OFFSET (:pageNumber * :pageSize) LIMIT * :pageSize", nativeQuery = true)
+    List<UserData> listAllUsers(int pageNumber, int pageSize);
+
+    @Query(value = "SELECT * FROM users", nativeQuery = true)
+    List<UserData> listWithoutPaging();
+
+
 }

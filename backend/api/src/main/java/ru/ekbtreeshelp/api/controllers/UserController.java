d82ea5b10dda55460233976bb314ae60cdb6d382
evin76 <gengerw1604@gmail.com>
2021-11-25 15:27:27 +0300

    commit 3 api module

--- backend/api/src/main/java/ru/ekbtreeshelp/api/controllers/UserController.java
+++ backend/api/src/main/java/ru/ekbtreeshelp/api/controllers/UserController.java
@@ -3,14 +3,13 @@ package ru.ekbtreeshelp.api.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.ekbtreeshelp.api.converter.UserConverter;
 import ru.ekbtreeshelp.api.dto.UserDto;
 import ru.ekbtreeshelp.api.service.SecurityService;
 import ru.ekbtreeshelp.api.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -33,5 +32,26 @@ public class UserController {
     public UserDto getSelf() {
         Long currentUserId = securityService.getCurrentUserId();
         return userConverter.toDto(userService.getById(currentUserId));
+    }
+
+    @GetMapping("/listUsersWithoutPaging")
+    public List<UserDto> listUsersWithoutPaging(){
+        return userService.listUsersWithoutPaging();
+    }
+
+    @GetMapping("/listUsers")
+    public List<UserDto> listUsers(@RequestParam Integer pageNumber, @RequestParam Integer pageSize){
+        return userService.listAllUsers(pageNumber, pageSize);
+    }
+
+    @PutMapping
+    public UserDto update(@RequestBody UserDto userData){
+        UserDto user = userService.update(userData);
+        return user;
+    }
+
+    @GetMapping("/block/{id}")
+    public void blockUser(@RequestBody UserDto userData){
+        userService.blockUser(userData.getId());
     }
 }

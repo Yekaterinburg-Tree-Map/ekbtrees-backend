1d03f710189ea50c718d24df931667354f82adb7
evin76 <gengerw1604@gmail.com>
2021-11-30 18:21:32 +0300

    user api

--- backend/core/src/main/java/ru/ekbtreeshelp/core/repository/UserRepository.java
+++ backend/core/src/main/java/ru/ekbtreeshelp/core/repository/UserRepository.java
@@ -1,5 +1,5 @@
 package ru.ekbtreeshelp.core.repository;
-
+//
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;

824510bb69c1f394d766cf964d684d87ac7dc3c0
evin76 <gengerw1604@gmail.com>
2021-11-30 18:31:14 +0300

    commit for admin

--- backend/admin/src/main/java/ru/ekbtreeshelp/admin/UsersController.java
+++ backend/admin/src/main/java/ru/ekbtreeshelp/admin/UsersController.java
@@ -24,17 +24,6 @@ public class UsersController {
         return usersService.listWithoutPaging();
     }
 
-    @GetMapping("/test")
-    List<UserData> listAllUsers(){
-        List<UserData> userList = new ArrayList<>();
-        UserData user1 = new UserData("name1", "name1", new Date(), 113133242L, false);
-        UserData user2 = new UserData("name2", "name2", new Date(), 323224343L, false);
-        userList.add(user1);
-        userList.add(user2);
-
-        return userList;
-    }
-
     @GetMapping("/{id}")
     UserData getByID(@PathVariable Long id){
         return usersService.getByID(id);
